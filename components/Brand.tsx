b08835097c14a0ee50859a7b2ec4282f6ad7f1f7
Quang Nguyễn <2jiro.jp@gmail.com>
2024-12-07 11:09:27 +0700

    update style

--- components/Brand.tsx
+++ components/Brand.tsx
@@ -3,7 +3,7 @@ export default function Brand() {
         <section className="">
             <div className="mx-auto max-w-[95%] ">
                 <h2
-                    className="all-caps-paragraph-1-5em text-center"
+                    className="all-caps-paragraph-1-5em text-center md:text-start"
                 >
                     Trusted by Visionaries &amp; Innovators
                 </h2>
