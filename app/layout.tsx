db8e69fa1786356065893ade1ffbc194d8b05561
Quang Nguyễn <2jiro.jp@gmail.com>
2024-12-11 22:14:39 +0700

    update seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 export const metadata: Metadata = {
     title: "@quangngx - Creative Developer",
-    keywords: ["quangngx", "quang.ngx", "quang", "quangnnx", "freelancer", "web freelancer", "gsap freelancer"],
+    keywords: ["quangngx", "quang.ngx", "quang", "quangnnx", "quangngx webflow","quang.ngx webflow", "freelancer", "web freelancer"],
     description: "I’m Quang Nguyễn (@quangngx), a designer and creative developer with 4 years of turning big ideas into things that actually look and work great. From designs that catch eyes to interactions that feel just right, I make sure every project stands out—and doesn’t put anyone to sleep.",
 };
 
