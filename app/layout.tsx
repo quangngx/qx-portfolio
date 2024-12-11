afbb465df296a6a66a290e77f94129b27a683545
Quang Nguyễn <2jiro.jp@gmail.com>
2024-12-11 22:10:09 +0700

    update SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,10 +4,8 @@ import React from "react";
 
 export const metadata: Metadata = {
     title: "@quangngx - Creative Developer",
-    keywords: ["quangngx","quang.ngx","freelancer","web freelancer","gsap freelancer"],
-    description: "Welcome to my creative space! I’m Quang Nguyễn, a Creative Developer, UI/UX Designer, and Web Developer with 4+ years of experience turning bold ideas into stunning digital experiences. From designing intuitive interfaces to building fast, responsive, and interactive websites, I work across a range of modern technologies to bring your vision to life. Whether it’s Webflow, custom frameworks, or the latest tools, I deliver work that looks amazing, feels seamless, and just works—no limits, no compromises.\n" +
-        "\n" +
-        "Let’s create something extraordinary together!",
+    keywords: ["quangngx", "quang.ngx", "freelancer", "web freelancer", "gsap freelancer"],
+    description: "I’m Quang Nguyễn (@quangngx), a designer and creative developer with 4 years of turning big ideas into things that actually look and work great. From designs that catch eyes to interactions that feel just right, I make sure every project stands out—and doesn’t put anyone to sleep.",
 };
 
 export default function RootLayout({children,}: Readonly<{ children: React.ReactNode; }>) {
